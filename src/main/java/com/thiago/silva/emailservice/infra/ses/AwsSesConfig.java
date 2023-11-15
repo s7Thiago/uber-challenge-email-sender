8e3f5283245797c673f4f4c237e982f167988fbb
thiago.silva <thiago.silva@opah.com.br>
2023-11-15 07:46:14 -0300

    chore: Classe de config (Bean) para instruir o spring sobre como instanciar o SES

---
+++ src/main/java/com/thiago/silva/emailservice/infra/ses/AwsSesConfig.java
@@ -0,0 +1,22 @@
+package com.thiago.silva.emailservice.infra.ses;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+/**
+ * Configura o Spring para instanciar corretamente a classe que integra com o serviço
+ * de e-mail da AWS
+ *
+*/
+@Configuration
+public class AwsSesConfig {
+
+    @Bean
+    AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+    }
+
+}
